9331085dc8d44c586e43d184dec436cee44a222f
Iaan Krynauw <iaan.krynauw@fuseuniversal.com>
2020-01-22 11:02:46 +0200

    sns delegation specs

--- spec/shoryuken/sns_spec.rb
+++ spec/shoryuken/sns_spec.rb
@@ -3,7 +3,11 @@ RSpec.describe Shoryuken::Sns do
     expect(Shoryuken::Sns::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'Shoryuken implements delegation method sns_client' do
+    Shoryuken.sns_client
+  end
+
+  it 'Shoryuken still implements old delegation methods' do
+    expect(Shoryuken.groups).to eq({})
   end
 end
